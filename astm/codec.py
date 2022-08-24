59634a0c76cd01f014aaf156e79c28049f0a0d60
Gat Sirivong <sirivong@gmail.com>
2022-08-24 16:17:33 -0700

    Add pyproject.toml; reference collections.abc.Iterable.

--- astm/codec.py
+++ astm/codec.py
@@ -7,7 +7,7 @@
 # you should have received as part of this distribution.
 #
 
-from collections import Iterable
+from collections.abc import Iterable
 from .compat import unicode
 from .constants import (
     STX, ETX, ETB, CR, LF, CRLF,
